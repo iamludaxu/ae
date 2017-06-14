7065444530c0e655096281f8433be808d8ac3c6b
iamludaxu <iamludaxu@163.com>
2017-06-14 16:05:56 +0800

    增加kotlin的基础使用

--- app/src/main/java/gift/witch/android/ae/kotlin/KotlinActivity.kt
+++ app/src/main/java/gift/witch/android/ae/kotlin/KotlinActivity.kt
@@ -1,15 +1,27 @@
 package gift.witch.android.ae.kotlin
 
 import android.os.Bundle
-
+import android.widget.TextView
 import gift.witch.android.ae.R
 import gift.witch.android.ae.base.BaseCompatActivity
 
+/**
+ *
+ */
 class KotlinActivity : BaseCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+        val textTV  = findViewById(R.id.text) as TextView
+        val btnBTN = findViewById(R.id.btn)
+
+        btnBTN.setOnClickListener {
+            textTV.setText("已经点击我了")
+        }
+
     }
 
 }
