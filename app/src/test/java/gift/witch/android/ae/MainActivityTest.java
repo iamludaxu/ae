48f7510607a790b1c66d803c4fc7a80addf81f64
iamludaxu <iamludaxu@163.com>
2017-06-21 09:30:11 +0800

    增加测试依赖

--- app/src/test/java/gift/witch/android/ae/MainActivityTest.java
+++ app/src/test/java/gift/witch/android/ae/MainActivityTest.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
+import gift.witch.android.ae.message.MessageActivity;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MyRobolectricTestRunner.class)
 @Config(manifest = "./src/main/AndroidManifest.xml",sdk = 23)
 public class MainActivityTest {
 
     @Test
     public void testMainActivity() {
-        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+        MessageActivity mainActivity = Robolectric.setupActivity(MessageActivity.class);
+        mainActivity.findViewById(R.id.btn1).performClick();
+        assertEquals(4, 2 + 1);
     }
+
 }
